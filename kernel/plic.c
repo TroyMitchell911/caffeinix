67e35cf21bfda90c40c9a8379efdf7b1dd16263f
TroyMitchell <andrew998@126.com>
2024-04-18 06:17:01 +0000

    Improve comments in plic.c

--- kernel/plic.c
+++ kernel/plic.c
@@ -11,15 +11,14 @@ void plic_init_hart(void)
 {
         int hart = cpuid();
   
-        // set enable bits for this hart's S-mode
-        // for the uart and virtio disk.
+        /* set enable bits for this hart's S-mode for the uart and virtio disk. */
         *(uint32*)PLIC_SENABLE(hart) = (1 << UART0_IRQ) | (1 << VIRTIO0_IRQ);
 
-        // set this hart's S-mode priority threshold to 0.
+        /* set this hart's S-mode priority threshold to 0. */
         *(uint32*)PLIC_SPRIORITY(hart) = 0;
 }
 
-// ask the PLIC what interrupt we should serve.
+/* ask the PLIC what interrupt we should serve. */
 int plic_claim(void)
 {
   int hart = cpuid();
@@ -27,7 +26,7 @@ int plic_claim(void)
   return irq;
 }
 
-// tell the PLIC we've served this IRQ.
+/* tell the PLIC we've served this IRQ. */
 void plic_complete(int irq)
 {
   int hart = cpuid();
