a4bdf7249768fc16a3cb458ddb197d34b8ed2942
GoKo-Son626 <wangshuqi626@126.com>
2024-05-19 02:43:58 +0000

    [Fixed]A indentation problem

--- mkfs/mkfs.c
+++ mkfs/mkfs.c
@@ -82,7 +82,7 @@ int main(int argc, char *argv[])
         static_assert(sizeof(int) == 4, "Integers must be 4 bytes!");
 
         if(argc < 2) {
-        fprintf(stderr, "Usage: mkfs fs.img files...\n");
+                fprintf(stderr, "Usage: mkfs fs.img files...\n");
                 exit(1);
         }
 
