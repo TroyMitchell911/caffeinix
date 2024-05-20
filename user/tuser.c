364914df19481d3cf7ca5a78df8f6d1295ac46f9
Troy <133468432+TroyMitchell911@users.noreply.github.com>
2024-05-20 11:51:27 +0800

    Added a test for `chdir`

--- user/tuser.c
+++ user/tuser.c
@@ -13,7 +13,21 @@
 
 int main(void)
 {
+        int ret;
         printf("test user program!\n");
-        
+rerun:
+        ret = chdir("test_dir");
+        if(ret != 0) {
+                printf("change directory to 'test_dir' failed\n");
+                ret = mkdir("test_dir");
+                if(ret != 0) {
+                        printf("create directory 'test_dir' failed\n");
+                } else {
+                        printf("create directory 'test_dir' successful\n");
+                        goto rerun;
+                }
+        } else {
+                printf("change directory to 'test_dir' successful\n");
+        }
         return 0;
 }
