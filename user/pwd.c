eb97f8c71e85c0b1daccd31128259d1a39c65a2a
GoKo-Son626 <wangshuqi626@126.com>
2024-09-13 08:12:31 +0000

    [FIXED]Modified the return of `pwd`

--- user/pwd.c
+++ user/pwd.c
@@ -13,7 +13,6 @@ int main(int argc, char *argv[]) {
         char cwd[MAXPATH];
 
         if (getcwd(cwd, MAXPATH)) {
-                printf("%s", "pwd:getcwd failed\n");
                 return -1;
         }
 
